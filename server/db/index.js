9a497c5115a9c3385e234b3a7057548f436753fc
Ruben <ruben@rubens-MacBook-Pro.local>
2021-05-10 15:25:03 -0400

    modified db connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,9 @@
 const pgp = require('pg-promise')();
 const db = pgp('postgres:///products');
+// const fs = require('fs');
+// const csv = require('csv-parser');
 
+// TESTING SERVER IS ONLINE
 db.one('Select $1 AS value', 123)
   .then(data => console.log('DATA: ', data.value))
   .catch(err => console.log('ERROR: ', error));
